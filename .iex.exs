df4c053edbe7021a9534cc6bd9d27585a34a9cc4
Robin Hilliard <robin@rocketboots.com>
2021-05-10 22:48:07 +1000

    Move API functions back to their related modules

--- .iex.exs
+++ .iex.exs
@@ -1,2 +1,8 @@
-import MAVLink.Util.API, only: [mavs: 0, focus: 0, focus: 1, focus: 2, params: 0, params: 1, params: 2]
-:observer.start()
+#  Using MAVProxy as a reference, provide helper functions for working with
+#  vehicles running APM. Importing the API into an iex session should give
+#  an interactive CLI very similar to MAVProxy, using info/warn level log
+#  messages for extra feedback. The same API can be called directly from code.
+
+import MAVLink.Util.CacheManager, only: [mavs: 0, params: 0, params: 1, params: 2]
+import MAVLink.Util.FocusManager, only: [focus: 0, focus: 1, focus: 2]
+
